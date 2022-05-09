06a0de7a545e24e448c7c58b2cd28ea151279ba2
julien basquin <julienbasquin.jb@gmail.com>
2022-05-09 15:32:54 +0200

    fixed bugs features

---
+++ src/Service/MessageGenerator.php
@@ -0,0 +1,30 @@
+<?php
+
+// src/Service/MessageGenerator.php
+namespace App\Service;
+
+use Psr\Log\LoggerInterface;
+
+class MessageGenerator
+{
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function getHappyMessage(): string
+    {
+        $this->logger->info('About to find a happy message!');
+        $messages = [
+            'You did it! You updated the system! Amazing!',
+            'That was one of the coolest updates I\'ve seen all day!',
+            'Great work! Keep going!',
+        ];
+
+        $index = array_rand($messages);
+
+        return $messages[$index];
+    }
+}
